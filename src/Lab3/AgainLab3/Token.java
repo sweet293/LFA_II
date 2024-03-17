166ac3ae2c3012c163d8b291cd0f45301947d7fd
Andreea Burdui <andreea.burdui@gmail.com>
2024-03-17 18:22:11 +0200

    "added the laboratory 4 files and pdf"

--- src/Lab3/AgainLab3/Token.java
+++ src/Lab3/AgainLab3/Token.java
@@ -7,6 +7,7 @@ class Token {
     public String match; //matches patters to their tokens. When a match is found,
     // it is extracted and analysed. Each character will be analysed. if there is a character after it,
     // it is also analysed and is made a conclusion : is it still an identifier or actually an operator?
+
     public Token(TokenType type, String match) {
         this.type = type;
         this.match = match;
